d2feb04bc4f35feceb579fce23c8b6e70fc0098f
fortunto2 <info@3dstr.ru>
2017-11-01 23:47:38 +0500

    Добавили категории через фильтертегс

---
+++ gazovik/webapp/templatetags/category.py
@@ -0,0 +1,37 @@
+from django import template
+
+from ..models import Tovar
+
+register = template.Library()
+
+
+@register.inclusion_tag('webapp/tags/tovar_list.html', takes_context=True)
+def category_list(context, cat_id, num):
+    """
+    Товары по категориям
+    """
+
+    print ('cat_id:', cat_id)
+
+    result = Tovar.objects.filter(category_id=cat_id).order_by('-id')[:num]
+
+    return {
+        'category': result
+    }
+
+
+# @register.inclusion_tag('webapp/tags/tovar_list.html', takes_context=True)
+# def category_list(context, cat_id, num):
+#     """
+#     Товары по категориям
+#     """
+#
+#     print ('cat_id:', cat_id)
+#
+#     result = Tovar.objects.filter(category_id=cat_id).order_by('-id')[:num]
+#
+#     # .select_related('author__profile__user').order_by('-created_at')[:num]
+#
+#     return {
+#         'category': result
+#     }
