445869e675d4618afd639378ddac5a3ac560de11
fortunto2 <info@3dstr.ru>
2017-11-06 22:08:45 +0500

    Исправили slick

--- gazovik/webapp/templatetags/category.py
+++ gazovik/webapp/templatetags/category.py
@@ -11,13 +11,25 @@ def category_list(context, cat_id, num):
     Товары по категориям
     """
 
-    print ('cat_id:', cat_id)
+    result = Tovar.objects.filter(category_id=cat_id).order_by('-id')[:num]
+
+    return {
+        'category': result
+    }
+
+
+@register.inclusion_tag('webapp/tags/tovar_list_slick.html', takes_context=True)
+def category_slick(context, cat_id, num):
+    """
+    Товары по категориям в виде слайдера
+    """
 
     result = Tovar.objects.filter(category_id=cat_id).order_by('-id')[:num]
 
     return {
         'category': result
     }
+
 
 
 # @register.inclusion_tag('webapp/tags/tovar_list.html', takes_context=True)
